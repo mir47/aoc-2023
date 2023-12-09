90b451bbd3a9b9e4da301648122c5bbe9aa186e6
miro <minev.miro@gmail.com>
2023-12-10 00:40:42 +0100

    Refactor

--- src/main/kotlin/days/Day08.kt
+++ src/main/kotlin/days/Day08.kt
@@ -1,19 +1,28 @@
 package days
 
 import util.lcm
-import util.println
-import util.readInput
 
-fun main() {
-    val day = "08"
+class Day08 : Day(8) {
 
-    fun parse(input: List<String>) = input.drop(2).associate {
+    override fun partOne(): Long {
+        val (instructions, network) = inputList[0] to parse(inputList)
+        return "AAA".solveForNode(instructions, network)
+    }
+
+    override fun partTwo(): Long {
+        val (instructions, network) = inputList[0] to parse(inputList)
+        return network.filter { it.key.endsWith('A') }
+            .map { it.key.solveForNode(instructions, network) }
+            .reduce(::lcm)
+    }
+
+    private fun parse(input: List<String>) = input.drop(2).associate {
         val line = it.split(" = ")
         val targets = line.last().drop(1).dropLast(1).split(", ")
         line.first() to mapOf('L' to targets.first(), 'R' to targets.last())
     }
 
-    fun String.solveForNode(
+    private fun String.solveForNode(
         instructions: String,
         nodes: Map<String, Map<Char, String>>
     ): Long {
@@ -25,26 +34,4 @@ fun main() {
         }
         return steps.toLong()
     }
-
-    fun part1(input: List<String>): Long {
-        val (instructions, network) = input[0] to parse(input)
-        return "AAA".solveForNode(instructions, network)
-    }
-
-    fun part2(input: List<String>): Long {
-        val (instructions, network) = input[0] to parse(input)
-        return network.filter { it.key.endsWith('A') }
-            .map { it.key.solveForNode(instructions, network) }
-            .reduce(::lcm)
-    }
-
-    val testInput1 = readInput("test_input_day_${day}_part_1")
-    val testInput2 = readInput("test_input_day_${day}_part_2")
-    val input = readInput("input_day_${day}")
-
-    check(part1(testInput1) == 6L)
-    part1(input).println()
-
-    check(part2(testInput2) == 6L)
-    part2(input).println()
 }
---
+++ src/test/kotlin/days/Day08Test.kt
@@ -0,0 +1,8 @@
+package days
+
+class Day08Test : DayTest() {
+
+    override val day = Day08()
+    override val part1 = 6L
+    override val part2 = 6L
+}
